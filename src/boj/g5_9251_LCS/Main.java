17f6b293fb2509baa06667930f1326e627311367
y <iareyouwhy@gmail.com>
2022-09-06 00:38:17 +0900

    2022-09-05-mon boj 9251

---
+++ src/boj/g5_9251_LCS/Main.java
@@ -0,0 +1,30 @@
+package boj.g5_9251_LCS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int[][] dp = new int[1001][1001];
+
+        for (int i=1; i<=str2.length(); i++) {
+            for (int j=1; j<=str1.length(); j++) {
+//                System.out.print(str2.charAt(i-1)+""+str1.charAt(j-1));
+                if (str2.charAt(i-1)==str1.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+//                    System.out.print(dp[i][j]+" ");
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+//                    System.out.print(dp[i][j]+" ");
+                }
+            }
+//            System.out.println();
+        }
+
+        System.out.println(dp[str2.length()][str1.length()]);
+    }
+}
