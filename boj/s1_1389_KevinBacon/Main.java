18e90fb55ff780371929ff8bd55eb72b53af0753
y <whyyouareay@gmail.com>
2022-12-14 14:29:06 +0900

    2022-12-14-wed boj 1389

--- boj/s1_1389_KevinBacon/Main.java
+++ boj/s1_1389_KevinBacon/Main.java
@@ -8,53 +8,52 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int W, H;
-    static int[][] banner;
-    static boolean[][] visited;
-    //우하좌상
-    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-    static int[] dx = {1, 1, 0, -1, -1, -1, 0, 1};
-    public static void main(String[] args) throws Exception{
+    static int N, M;
+    static int[][] adj;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        H = Integer.parseInt(st.nextToken());
-        W = Integer.parseInt(st.nextToken());
-        banner = new int[H][W];
-        visited = new boolean[H][W];
-        for (int h=0; h<H; h++) {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        adj = new int[N][N];
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                if (i==j) adj[i][j] = 0;
+//                else adj[i][j] = Integer.MAX_VALUE;
+                else adj[i][j] = 1000000000;
+            }
+        }
+        for (int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int w=0; w<W; w++) {
-                banner[h][w] = Integer.parseInt(st.nextToken());
-            }
-        }
-        // End of input
-
-        int answer = 0;
-        for (int i=0; i<H; i++) {
-            for (int j=0; j<W; j++) {
-                if (banner[i][j]==0 || visited[i][j]) continue;
-                bfs(i, j);
-                answer++;
-            }
+            int user1 = Integer.parseInt(st.nextToken())-1;
+            int user2 = Integer.parseInt(st.nextToken())-1;
+            adj[user1][user2] = 1;
+            adj[user2][user1] = 1;
         }
 
-        System.out.println(answer);
-    }
-
-    static void bfs(int startY, int startX) {
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{startY, startX});
-        visited[startY][startX] = true;
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            for (int d=0; d<8; d++) {
-                int[] next = new int[]{curr[0]+dy[d], curr[1]+dx[d]};
-                if (next[0]<0 || next[0]>=H || next[1]<0 || next[1]>=W || visited[next[0]][next[1]]) continue;
-                if (banner[next[0]][next[1]]==1) {
-                    visited[next[0]][next[1]] = true;
-                    q.add(next);
+        for (int k=0; k<N; k++) {
+            for (int i=0; i<N; i++) {
+                if (i==k) continue;
+                for (int j=0; j<N; j++) {
+                    if (i==j || k==j) continue;
+                    if (adj[i][j] > adj[i][k]+adj[k][j]) {
+                        adj[i][j] = adj[i][k]+adj[k][j];
+                    }
                 }
             }
         }
+        int[] kevinBaconNum = new int[N];
+        int min = Integer.MAX_VALUE;
+        int answer = 0;
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                kevinBaconNum[i] += adj[i][j];
+            }
+            if (kevinBaconNum[i]<min) {
+                answer = i+1;
+                min = kevinBaconNum[i];
+            }
+        }
+        System.out.println(answer);
     }
 }
