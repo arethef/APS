ff7827888c42133b9878ffae0d1a917d59708967
y <whyyouareay@gmail.com>
2023-01-08 23:39:48 +0900

    2023-01-08-sun boj 12904

---
+++ boj/g5_12904_ANB/Main.java
@@ -0,0 +1,24 @@
+package g5_12904_ANB;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder s = new StringBuilder(br.readLine());
+        StringBuilder t = new StringBuilder(br.readLine());
+
+        while (s.length()<t.length()) {
+            if (t.charAt(t.length()-1)=='A') {
+                t.deleteCharAt(t.length()-1);
+            } else if (t.charAt(t.length()-1)=='B') {
+                t.deleteCharAt(t.length()-1);
+                t.reverse();
+            }
+        }
+        if (s.toString().equals(t.toString())) System.out.println(1);
+        else System.out.println(0);
+
+    }
+}
