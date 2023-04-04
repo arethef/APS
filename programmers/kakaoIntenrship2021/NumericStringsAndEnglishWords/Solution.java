f8c294a5f7352c006ac70a99fbbf43dc24f47252
y <whyyouareay@gmail.com>
2023-04-04 15:47:25 +0900

    2022-09-22-thu programmers 2021 카카오 채용연계형 인턴십 - 숫자 문자열과 영단어

--- programmers/kakaoIntenrship2021/NumericStringsAndEnglishWords/Solution.java
+++ programmers/kakaoIntenrship2021/NumericStringsAndEnglishWords/Solution.java
@@ -1,9 +1,5 @@
 package kakaoIntenrship2021.NumericStringsAndEnglishWords;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 public class Solution {
     public static void main(String[] args) {
         String s = "one4seveneight";
