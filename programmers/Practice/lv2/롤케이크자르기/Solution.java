908b34986406f3ced7d20064464af893fc0a751f
y <whyyouareay@gmail.com>
2023-05-06 18:18:57 +0900

    2023-05-06-sat programmers lv2 연습문제 - 롤케이크 자르기

---
+++ programmers/Practice/lv2/롤케이크자르기/Solution.java
@@ -0,0 +1,31 @@
+package Practice.lv2.롤케이크자르기;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 1, 3, 1, 4, 1, 2}));
+    }
+
+    static int solution(int[] topping) {
+        int answer = 0;
+        int N = topping.length;
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
+        int[] a = new int[N];
+        int[] b = new int[N];
+        for (int i=0; i<N; i++) {
+            setA.add(topping[i]);
+            setB.add(topping[N-1-i]);
+            a[i] = setA.size();
+            b[N-1-i] = setB.size();
+        }
+        for (int i=0; i<N-1; i++) {
+            if (a[i]==b[i+1]) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
