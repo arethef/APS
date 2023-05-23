493fd8a41409641f5b910636a16c0ac5a4d49b4a
pound <llattely2@gmail.com>
2023-05-24 00:34:25 +0900

    2023-05-23-tue programmers lv3 탐욕법(Greedy) - 단속카메라

---
+++ programmers/탐욕법Greedy/lv3/단속카메라/Solution.java
@@ -0,0 +1,31 @@
+package 탐욕법Greedy.lv3.단속카메라;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[][] routes = {
+                {-20,-15},
+                {-14,-5},
+                {-18,-13},
+                {-5,-3}
+        };
+        System.out.println(solution(routes));
+    }
+    static int solution(int[][] routes) {
+        int answer = 0;
+        Arrays.sort(routes, (a, b) -> a[1]-b[1]);
+        for (int[] r : routes) {
+            System.out.println(Arrays.toString(r));
+        }
+        int c = -30001;
+        for (int[] r : routes) {
+            if (r[0]<=c) {
+                continue;
+            }
+            answer++;
+            c = r[1];
+        }
+        return answer;
+    }
+}
